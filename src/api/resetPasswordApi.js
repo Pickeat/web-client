397e3127dcb77182628d9a127921596c527250f4
Mahé Caurette <mahe.caurette@epitech.eu>
2020-10-06 01:34:10 +0200

    Reset Password route (not good request)

---
+++ src/api/resetPasswordApi.js
@@ -0,0 +1,34 @@
+import setAxiosConfig from "../helpers/setAxiosConfig";
+import axios from "axios";
+import {toast} from "react-toastify";
+import {RESET_PASSWORD_URL} from '../constants/apiEndpoints';
+import Cookies from 'js-cookie';
+
+
+export default async function resetPasswordApi(password, confirmPassword) {
+    let body = {
+        'password': password,
+    };
+    let config = setAxiosConfig('POST', RESET_PASSWORD_URL, true);
+
+    config['data'] = body;
+    if (!password) {
+        toast.error("password field is blank");
+        return;
+    }
+    if (password !== confirmPassword) {
+        toast.error("The two password are not identical");
+        return;
+    }
+    return await axios(config).then((response) => {
+
+        if (response.status === 200) {
+            Cookies.set('jwt', response.data.access_token.token);
+            return response.data;
+        } else {
+            toast.warn(response.data.message);
+        }
+    }).catch((error) => {
+        toast.error(error.response.data.description);
+    });
+}
---
+++ src/route/ResetPassword.js
@@ -0,0 +1,135 @@
+import React, {useState} from 'react';
+import Avatar from '@material-ui/core/Avatar';
+import Button from '@material-ui/core/Button';
+import Logo from '../assets/logo.png';
+import Typography from '@material-ui/core/Typography';
+import {makeStyles} from '@material-ui/core/styles';
+import { PickeatTextField } from '../components/PickeatTextField';
+import Background from '../components/Background';
+import backgroundSrc from '../assets/wallpaper-login.jpg';
+import resetPasswordApi from '../api/resetPasswordApi';
+import Paper from '@material-ui/core/Paper';
+import {Link} from "react-router-dom";
+
+const useStyles = makeStyles((theme) => ({
+    main: {
+        paddingTop: '5rem',
+        boxSizing: 'border-box',
+        height: '100vh',
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+        justifyContent: 'center'
+    },
+    container: {
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+        justifyContent: 'center',
+        height: '60%',
+        width: '40%',
+    },
+    avatar: {
+        backgroundColor: 'transparent',
+        height: 'auto',
+        width: '15%',
+        marginBottom: '2%'
+    },
+    form: {
+        width: '70%', // Fix IE 11 issue.
+        height: "50%",
+        display: "flex",
+        flexDirection: "column",
+        alignItems: 'center',
+        justifyContent: "space-evenly",
+    },
+}));
+
+export default function ResetPassword() {
+    const classes = useStyles();
+    const [password, setPassword] = useState("");
+    const [confirmPassword, setConfirmPassword] = useState('');
+    const [emailSent, setEmailSent] = useState(false);
+
+    const buildPaper = () => {
+        if (!emailSent) {
+            return (
+                <>
+                    <Avatar className={classes.avatar}>
+                        <img style={{ maxWidth: '100%', maxHeight: '100%'}} alt="PickEat Logo" src={Logo}/>
+                    </Avatar>
+                    <Typography component="h1" variant="h5">
+                        Forgot Password
+                    </Typography>
+                    <form className={classes.form}>
+                        <PickeatTextField
+                            variant="outlined"
+                            margin="normal"
+                            required
+                            fullWidth
+                            name="password"
+                            label="Password"
+                            type="password"
+                            id="password"
+                            value={password}
+                            onChange={(event => setPassword(event.target.value))}
+                        />
+                        <PickeatTextField
+                            variant="outlined"
+                            margin="normal"
+                            required
+                            fullWidth
+                            name="confirm-password"
+                            label="Confirm Password"
+                            type="password"
+                            id="confirm-password"
+                            value={confirmPassword}
+                            onChange={(event => setConfirmPassword(event.target.value))}
+                        />
+                        <Button
+                            style={{width: '50%'}}
+                            type="submit"
+                            variant="contained"
+                            color="primary"
+                            onClick={(e) => {
+                                e.stopPropagation();
+                                e.preventDefault();
+                                resetPasswordCall(password)
+                            }}
+                            className="pickeatBtn"
+                        >
+                            Reset Password
+                        </Button>
+                    </form>
+                </>
+            )
+            } else {
+                return (
+                    <>
+                        <Avatar className={classes.avatar}>
+                            <img style={{ maxWidth: '100%', maxHeight: '100%'}} alt="PickEat Logo" src={Logo}/>
+                        </Avatar>
+                        <Typography component="h1" variant="h5">
+                            Password has been reset !
+                        </Typography>
+                    </>
+                )
+            }
+    }
+
+    const resetPasswordCall = (password, confirmPassword) => {
+        setEmailSent(true);
+        resetPasswordApi(password, confirmPassword).then((response) => {
+            console.log(response);
+        });
+    };
+
+    return (
+        <div className={classes.main}>
+            <Background src={backgroundSrc}/>
+            <Paper elevation={24} className={classes.container}>
+                {buildPaper()}
+            </Paper>
+        </div>
+    );
+}
