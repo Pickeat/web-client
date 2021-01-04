4babb8a88777b41035b4f80a03c4fc54aad966cb
Mahe Caurette <mahe.caurette@epitech.eu>
2021-01-04 12:37:17 +0100

    [+] edit product when owning product

---
+++ src/api/getUserMeApi.js
@@ -0,0 +1,23 @@
+import setAxiosConfig from "../helpers/setAxiosConfig";
+import axios from "axios";
+import {toast} from "react-toastify";
+import { GET_USER_ME} from '../constants/apiEndpoints';
+import handleErrorToast from '../helpers/handleErrorToast';
+import Cookies from 'js-cookie';
+
+export default async function getUserMeApi() {
+    let config = setAxiosConfig('GET', `${GET_USER_ME}`, false);
+    let token = Cookies.get('jwt');
+
+    config['headers']['Authorization'] = `Bearer ${token}`;
+
+    return await axios(config).then((response) => {
+        if (response.status === 200) {
+            return response.data;
+        } else {
+            toast.warn(response.data.message);
+        }
+    }).catch((error) => {
+        handleErrorToast(error);
+    });
+}
