043b2fd8ee69ca433bc195873cb287f9c85b3718
Shegaie <gregoire.lanfranchini@epitech.eu>
2021-05-30 18:47:46 +0200

    remove previous tailwind installation

--- src/index.js
+++ src/index.js
@@ -4,7 +4,6 @@ import './index.css';
 import "./styles/tailwind.generated.css"
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-import './tailwind.output.css';
 
 ReactDOM.render(
     <React.StrictMode>
