8cd310d1c65ea4547d251cfa651b268c78eb8c4f
Shegaie <gregoire.lanfranchini@epitech.eu>
2021-09-07 14:10:28 +0200

    Chat: Set message in list when send

--- src/components/MessageSection.js
+++ src/components/MessageSection.js
@@ -52,6 +52,11 @@ export default function (props) {
             const user_id = Cookies.get('user_id');
             const to = props?.room?.contactId;
             socket.emit("sendMessage", {to, from: user_id, message})
+            setAllMessages([...allMessages, {
+                from: {_id: user_id},
+                message,
+                to: {_id: props?.room?.contactId}
+            }])
             setMessage("");
         }
     }
@@ -82,26 +87,26 @@ export default function (props) {
                 <div ref={messagesContainerRef}/>
             </div>
             {!isEmpty(props.room) &&
-                <form noValidate onSubmit={(e) => {
-                    sendMessage(e)
-                }}>
-                    <div className="absolute inset-x-0 bottom-0 flex flex-row m-2">
-                        <input type="text" name="message" id="message" placeholder="Type your message"
-                               className="shadow-sm focus:ring-green-500 focus:border-green-500 block w-full sm:text-sm border-gray-300 rounded-md"
-                               onChange={(event => setMessage(event.target.value))} value={message}/>
-                        <button onClick={(e) => {
-                            sendMessage(e)
-                        }} type="button"
-                                className="inline-flex items-center px-3 py-1.5 ml-4 border border-transparent shadow-sm text-sm leading-4 rounded-md text-white bg-green-600 hover:bg-green-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-green-500">
-                            Send
-                            <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5 ml-2 transform rotate-90"
-                                 viewBox="0 0 20 20" fill="currentColor">
-                                <path
-                                    d="M10.894 2.553a1 1 0 00-1.788 0l-7 14a1 1 0 001.169 1.409l5-1.429A1 1 0 009 15.571V11a1 1 0 112 0v4.571a1 1 0 00.725.962l5 1.428a1 1 0 001.17-1.408l-7-14z"/>
-                            </svg>
-                        </button>
-                    </div>
-                </form>
+            <form noValidate onSubmit={(e) => {
+                sendMessage(e)
+            }}>
+                <div className="absolute inset-x-0 bottom-0 flex flex-row m-2">
+                    <input type="text" name="message" id="message" placeholder="Type your message"
+                           className="shadow-sm focus:ring-green-500 focus:border-green-500 block w-full sm:text-sm border-gray-300 rounded-md"
+                           onChange={(event => setMessage(event.target.value))} value={message}/>
+                    <button onClick={(e) => {
+                        sendMessage(e)
+                    }} type="button"
+                            className="inline-flex items-center px-3 py-1.5 ml-4 border border-transparent shadow-sm text-sm leading-4 rounded-md text-white bg-green-600 hover:bg-green-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-green-500">
+                        Send
+                        <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5 ml-2 transform rotate-90"
+                             viewBox="0 0 20 20" fill="currentColor">
+                            <path
+                                d="M10.894 2.553a1 1 0 00-1.788 0l-7 14a1 1 0 001.169 1.409l5-1.429A1 1 0 009 15.571V11a1 1 0 112 0v4.571a1 1 0 00.725.962l5 1.428a1 1 0 001.17-1.408l-7-14z"/>
+                        </svg>
+                    </button>
+                </div>
+            </form>
             }
         </div>
     )
