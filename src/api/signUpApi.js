1136644d0897d00e01ef97456b95700a530802fe
Pierre <pierre.chaudron@epitech.eu>
2021-09-30 00:44:03 +0200

    feat(register): added field to choose name at register

--- src/api/signUpApi.js
+++ src/api/signUpApi.js
@@ -1,41 +1,43 @@
-import setAxiosConfig from "../helpers/setAxiosConfig";
-import axios from "axios";
-import {toast} from "react-toastify";
-import {SIGN_UP_URL} from '../constants/apiEndpoints';
-import Cookies from 'js-cookie'
+import setAxiosConfig from '../helpers/setAxiosConfig';
+import axios from 'axios';
+import { toast } from 'react-toastify';
+import { SIGN_UP_URL } from '../constants/apiEndpoints';
+import Cookies from 'js-cookie';
 import handleErrorToast from '../helpers/handleErrorToast';
 
+export default async function signUpApi(email, password, confirmPassword, phone, age, name) {
+  let body = {
+    email: email,
+    password: password,
+    phone_number: phone,
+    age: age,
+    name: name,
+  };
+  let config = setAxiosConfig('POST', SIGN_UP_URL, true);
 
-export default async function signUpApi(email, password, confirmPassword, phone, age) {
-    let body = {
-        'email': email,
-        'password': password,
-        'phone_number': phone,
-        'age': age
-    };
-    let config = setAxiosConfig('POST', SIGN_UP_URL, true);
-
-    config['data'] = body;
-    if (password !== confirmPassword) {
-        toast.error("The two password are not identical");
-        return false;
-    }
-    if (!email || !password || !age) {
-        toast.error("One or more field(s) is/are blank");
-        return false;
-    }
-    if (age < 18) {
-        toast.error("You must be over 18 to register on Pickeat");
-        return false;
-    }
-    return await axios(config).then((response) => {
-        if (response.status === 200) {
-            Cookies.set('jwt', response.data.access_token.token);
-            return true;
-        } else {
-            toast.warn(response.data.message);
-        }
-    }).catch((error) => {
-        handleErrorToast(error);
+  config['data'] = body;
+  if (password !== confirmPassword) {
+    toast.error('The two password are not identical');
+    return false;
+  }
+  if (!email || !password || !age) {
+    toast.error('One or more field(s) is/are blank');
+    return false;
+  }
+  if (age < 18) {
+    toast.error('You must be over 18 to register on Pickeat');
+    return false;
+  }
+  return await axios(config)
+    .then((response) => {
+      if (response.status === 200) {
+        Cookies.set('jwt', response.data.access_token.token);
+        return true;
+      } else {
+        toast.warn(response.data.message);
+      }
+    })
+    .catch((error) => {
+      handleErrorToast(error);
     });
 }
